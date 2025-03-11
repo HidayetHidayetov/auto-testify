a35d6991bcff137186adbf841d1920884847048e
HidayetHidayetov <hidayet.hidayetov@gmail.com>
2025-03-11 12:33:01 +0400

    Add common validation rules (numeric, integer, in, not_in, regex, date, url) to ValidationTestGenerator

--- src/Services/ValidationTestGenerator.php
+++ src/Services/ValidationTestGenerator.php
@@ -28,6 +28,41 @@ class ValidationTestGenerator
             'attributes_method' => 'getMinAttributes',
             'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
         ],
+        'numeric' => [
+            'template' => "test_{model}_{field}_must_be_numeric",
+            'attributes_method' => 'getNumericAttributes',
+            'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
+        ],
+        'integer' => [
+            'template' => "test_{model}_{field}_must_be_an_integer",
+            'attributes_method' => 'getIntegerAttributes',
+            'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
+        ],
+        'in' => [
+            'template' => "test_{model}_{field}_must_be_in_allowed_values",
+            'attributes_method' => 'getInAttributes',
+            'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
+        ],
+        'not_in' => [
+            'template' => "test_{model}_{field}_must_not_be_in_disallowed_values",
+            'attributes_method' => 'getNotInAttributes',
+            'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
+        ],
+        'regex' => [
+            'template' => "test_{model}_{field}_must_match_regex_pattern",
+            'attributes_method' => 'getRegexAttributes',
+            'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
+        ],
+        'date' => [
+            'template' => "test_{model}_{field}_must_be_a_valid_date",
+            'attributes_method' => 'getDateAttributes',
+            'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
+        ],
+        'url' => [
+            'template' => "test_{model}_{field}_must_be_a_valid_url",
+            'attributes_method' => 'getUrlAttributes',
+            'assertions' => "\$this->assertTrue(\$validator->fails());\n        \$this->assertArrayHasKey('{field}', \$validator->errors()->toArray());"
+        ],
     ];
 
     /**
@@ -125,5 +160,85 @@ class ValidationTestGenerator
     protected function getMinAttributes(string $field, array $attributes, ?string $param): array
     {
         return array_merge($attributes, [$field => str_repeat('a', (int)$param - 1)]);
+    }
+
+    /**
+     * @param string $field
+     * @param array<string, string> $attributes
+     * @param mixed $param
+     * @return array<string, string>
+     */
+    protected function getNumericAttributes(string $field, array $attributes, $param = null): array
+    {
+        return array_merge($attributes, [$field => 'not-a-number']);
+    }
+
+    /**
+     * @param string $field
+     * @param array<string, string> $attributes
+     * @param mixed $param
+     * @return array<string, string>
+     */
+    protected function getIntegerAttributes(string $field, array $attributes, $param = null): array
+    {
+        return array_merge($attributes, [$field => '12.34']); // Tam ədəd deyil
+    }
+
+    /**
+     * @param string $field
+     * @param array<string, string> $attributes
+     * @param string|null $param
+     * @return array<string, string>
+     */
+    protected function getInAttributes(string $field, array $attributes, ?string $param): array
+    {
+        $allowedValues = explode(',', $param ?? '');
+        return array_merge($attributes, [$field => 'invalid-value']); // Siyahıda olmayan dəyər
+    }
+
+    /**
+     * @param string $field
+     * @param array<string, string> $attributes
+     * @param string|null $param
+     * @return array<string, string>
+     */
+    protected function getNotInAttributes(string $field, array $attributes, ?string $param): array
+    {
+        $disallowedValues = explode(',', $param ?? '');
+        return array_merge($attributes, [$field => $disallowedValues[0] ?? 'disallowed']); // Siyahıda olan dəyər
+    }
+
+    /**
+     * @param string $field
+     * @param array<string, string> $attributes
+     * @param string|null $param
+     * @return array<string, string>
+     */
+    protected function getRegexAttributes(string $field, array $attributes, ?string $param): array
+    {
+        // Sadə bir nümunə: /^[a-z]+$/ (yalnız kiçik hərflər)
+        return array_merge($attributes, [$field => '123']); // Regex-ə uyğun olmayan dəyər
+    }
+
+    /**
+     * @param string $field
+     * @param array<string, string> $attributes
+     * @param mixed $param
+     * @return array<string, string>
+     */
+    protected function getDateAttributes(string $field, array $attributes, $param = null): array
+    {
+        return array_merge($attributes, [$field => 'invalid-date']);
+    }
+
+    /**
+     * @param string $field
+     * @param array<string, string> $attributes
+     * @param mixed $param
+     * @return array<string, string>
+     */
+    protected function getUrlAttributes(string $field, array $attributes, $param = null): array
+    {
+        return array_merge($attributes, [$field => 'not-a-url']);
     }
 }
